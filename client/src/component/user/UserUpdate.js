e95819109bfd2550178c72b017cf35ec71123817
limjeahyuk <dlawo9368@email.daelim.ac.kr>
2022-07-26 16:25:47 +0900

    post쪽 state 줄이기 구현 완료

--- client/src/component/user/UserUpdate.js
+++ client/src/component/user/UserUpdate.js
@@ -12,11 +12,6 @@ const UserUpdate = ({ name, isLoginCheck }) => {
     const [email, setEmail] = useState('');
     const [address, setAddress] = useState('');
 
-    // user data 가져오기
-    const [userNick, setUserNick] = useState('');
-    const [userPw, setUserPw] = useState('');
-    const [userEmail, setUserEmail] = useState('');
-    const [userAddress, setUserAddress] = useState('');
 
     const [data, setData] = useState({})
 
@@ -24,25 +19,21 @@ const UserUpdate = ({ name, isLoginCheck }) => {
 
     // input 창 관리
     const nickChangeHandler = (e) => {
-        setNickName(e.target.value);
+        setNickName(e.target.value)
         setData({
             ...data,
             usernick: e.target.value
         })
-        if (e.target.value === "") {
-            setData({
-                ...data,
-                usernick: userNick
-            })
-        }
     }
+
     const nickClickHandler = (e) => {
-        setNickName('');
+        setNickName("");
         setData({
             ...data,
-            usernick: userNick
+            usernick: ""
         })
     }
+
     const passwordHandler = (e) => {
         setPassword(e.target.value);
         setPasswordCheck('');
@@ -59,7 +50,7 @@ const UserUpdate = ({ name, isLoginCheck }) => {
             setPasswordChk(false);
             setData({
                 ...data,
-                userpw: userPw
+                userpw: ""
             })
         }
     }
@@ -74,25 +65,30 @@ const UserUpdate = ({ name, isLoginCheck }) => {
         setEmail('');
         setData({
             ...data,
-            email: userEmail+"@"+userAddress
+            email: ""
         })
     }
     const addressChangeHandler = (e) => {
         setAddress(e.target.value)
-        setData({
+        if (email) {
+            setData({
             ...data,
             email: email+"@"+e.target.value
         })
+        } else {
+            setData({
+                ...data,
+                email: ""
+            })
+        }
+        
+        
     }
 
     // 유저 정보 가져오고 기본값 설정.
     const sendRequest = async () => {
         const response = await axios.get(`http://localhost:8080/user/${name}`);
-        setUserNick(response.data[0].usernick);
         setNickName(response.data[0].usernick);
-        setUserPw(response.data[0].userpw);
-        setUserEmail(response.data[0].email.split('@')[0]);
-        setUserAddress(response.data[0].email.split('@')[1]);
         setEmail(response.data[0].email.split('@')[0]);
         setAddress(response.data[0].email.split('@')[1]);
         setData({
@@ -133,7 +129,7 @@ const UserUpdate = ({ name, isLoginCheck }) => {
             <form onSubmit={updateSubmitHandler}>
                 <div>
                     <label>닉네임 변경</label>
-                    <input type="text" onChange={nickChangeHandler} value={nickName} minLength="1" onClick={nickClickHandler} />
+                    <input type="text" onChange={nickChangeHandler} minLength="1" value={nickName} onClick={nickClickHandler} />
                 </div>
                 <div>
                     <label>비밀번호 변경</label>
@@ -149,7 +145,7 @@ const UserUpdate = ({ name, isLoginCheck }) => {
                 <div className={classes.emailbox}>
                     <label>이메일 변경</label>
                     <div className={classes.email}>
-                        <input type="text" value={email} onChange={emailChangeHandler} onClick={emailClickHandler} />@ 
+                        <input type="text" value={email} onChange={emailChangeHandler} onClick={emailClickHandler}  />@ 
                         <select value={address} onChange={addressChangeHandler}>
                             <option value="naver.com">naver.com</option>
                             <option value="nate.com">nate.com</option>
